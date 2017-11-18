2100a0adf88601cb16c4df1c75d204be91205d66
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2017-11-18 10:29:41 -0500

    Added gitignore and corrected mistake in sensorLogging

--- sensorLogging.cpp
+++ sensorLogging.cpp
@@ -19,13 +19,13 @@ const std::string currentDateTime() {
   return buf;
 }
 
-float processSensor(int ouput[CHAR_BIT]){
+float processSensor(int output[CHAR_BIT]){
   /*
     pre-process sensor data
   */
   float val = 0.4 * output[0] + 0.15 * (output[1]+output[2]+output[3]+output[4]);
   if(val > 0.8){
-    return val;
+    return 1;
   }
   return 0;
 }
@@ -41,24 +41,40 @@ float summation(int buffer[]){
   return sum;
 }
 
+char getSensorData(){
+  char j = 0;
+  for(int i = 0; i < 5; i++){
+    j *= 2;
+    j += rand()%2;
+  }
+  return j;
+}
+
 void logger(){
   /*
   infinite loop of sensor reading and writing to file after processing
   */
   ofstream ofs;
-  int buffer[10];
+  int buffer[10] = {0,0,0,0,0,0,0,0,0,0};
   int j = 0;
   ofs.open ("movement.csv", ofstream::out | ofstream::app);
   ofs<<"date,movement\n";
   while(true){
     char val = getSensorData();
-    int output[5];
+    int output[5] = {0,0,0,0,0} ;
+    /*
+      output[5] = {FSR, sensor1, sensor2, sensor3, sensor4}
+      value     = {sensor4, sensor3, sensor2, sensor1, FSR}
+    */
     for (int i = 0; i < 5; ++i) {
       output[i] = (val >> i) & 1;
     }
     buffer[j] = processSensor(output);
+    cout<<buffer[j];
+    cout<<endl;
     j++;
     if(j > 9){
+      cout<<endl;
       j = 0;
       float sum = summation(buffer);
       ofs<<currentDateTime()<<","<<sum<<"\n";
